7cea7d560f3a85f19153f8b744c2d637320758b4
yqk <yqk_work@163.com>
2022-07-06 15:23:37 +0800

    feat：master2

--- TestGit/TestGit/SceneDelegate.m
+++ TestGit/TestGit/SceneDelegate.m
@@ -19,6 +19,7 @@
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     // master 1
+//    master 2
 }
 
 
